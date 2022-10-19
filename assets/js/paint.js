c7e3d341965c2cfce5e0086e6d9ddc653e2928bf
Andrew <andrewcrink@gmail.com>
2022-10-19 12:58:49 -0400

    fixed opacity issue, needed ! in front of this

--- assets/js/paint.js
+++ assets/js/paint.js
@@ -3,14 +3,14 @@ function configureListeners() {
 
 
      for (var i = 0; i < images.length; i++) { 
-        document.getElementById(images[i].id).addEventListener('mouseover', addOpacity(), false)       
+        document.getElementById(images[i].id).addEventListener('mouseover', addOpacity, false)       
         // iterate over images and add mouseover event listeners      
-        document.getElementById(images[i].id).addEventListener('mouseout', removeOpacity(),false)
+        document.getElementById(images[i].id).addEventListener('mouseout', removeOpacity,false)
     } 
 }
 
 function addOpacity(event) {
-    if (this.classList.contains('dim')) {
+    if (!this.classList.contains('dim')) {
         this.classList.add('dim')
     }
     // add appropriate CSS class
